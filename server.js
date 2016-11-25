00f886e4144fd67aa407cd44e95b00b8975cd9c0
ShiHan Wan <shihanwan@gmail.com>
2016-11-25 18:41:45 -0500

    fixed DB reloading bug and made loading more efficient by sending only new task to client

--- server.js
+++ server.js
@@ -1,41 +1,41 @@
-// FIXME: Feel free to remove this :-)
-console.log('\n\nGood Luck! 😅\n\n');
-
 const server = require('socket.io')();
 const firstTodos = require('./data');
 const Todo = require('./todo');
 
+// This is going to be our fake 'database' for this application
+// Parse all default Todo's from db
+const DB = firstTodos.map((t) => {
+    // Form new Todo objects      
+    return new Todo(title = t.title);
+});
+
+// Sends a message to the client to reload all todos
+const loadTodos = () => {
+    server.emit('load', DB);
+}
+
+const refresh = (t) => {
+    server.emit('refresh', t);
+}
+
 server.on('connection', (client) => {
-    // This is going to be our fake 'database' for this application
-    // Parse all default Todo's from db
-
-    // FIXME: DB is reloading on client refresh. It should be persistent on new client connections from the last time the server was run...
-    const DB = firstTodos.map((t) => {
-        // Form new Todo objects
-        return new Todo(title=t.title);
-    });
-
-    // Sends a message to the client to reload all todos
-    const reloadTodos = () => {
-        server.emit('load', DB);
-    }
+    // Send the DB downstream on connect
+    loadTodos();
 
     // Accepts when a client makes a new todo
-    client.on('make', (t) => {
+    client.on('addTask', (t) => {
         // Make a new todo
-        const newTodo = new Todo(title=t.title);
+        const newTodo = new Todo(title = t.title);
 
         // Push this newly created todo to our database
         DB.push(newTodo);
 
-        // Send the latest todos to the client
-        // FIXME: This sends all todos every time, could this be more efficient?
-        reloadTodos();
+        console.log(DB)
+        // Send the new todo to the client
+        refresh([newTodo]);
     });
-
-    // Send the DB downstream on connect
-    reloadTodos();
 });
 
 console.log('Waiting for clients to connect');
 server.listen(3003);
+
