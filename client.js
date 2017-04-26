3c8fed94ed38910a4f911c2452f790e58b7dea54
Daria Sirous <daria.sirous@gmail.com>
2017-04-26 18:33:28 -0400

    Adds basic functionality for caching

--- client.js
+++ client.js
@@ -2,6 +2,7 @@ const server = io('http://localhost:3003/');
 const list = document.getElementById('todo-list');
 
 var AdditionOrder = Object.freeze({ APPEND: 0, PREPEND: 1});
+var isFirstConnection = true;
 
 document.body.onload = function() {
    var input = document.getElementById('todo-input');
@@ -76,33 +77,88 @@ function render(todo, ao = AdditionOrder.APPEND) {
 // This event is for (re)loading the entire list of todos from the server
 server.on('load', (todos) => {
     list.innerHTML = '';
-    todos.forEach((todo) => render(todo));
+    cachClear();
+    todos.forEach((todo) => {
+        render(todo);
+        cachAddItem(todo);
+    });
 });
+
+// This event is to report that connection was lost
+server.on('connect_error', () => {
+    if (isFirstConnection)
+        cachGet().forEach((todo) => render(todo));
+    isFirstConnection = false;
+})
 
 // This event is for the addition of new Todo item to the list
 server.on('prepend', (todo) => {
     render(todo, AdditionOrder.PREPEND);
+    cachAddItem(todo);
 });
 
 // This event removes todo by id
 server.on('remove', (id) => {
     listItem = document.getElementById(id);
     list.removeChild(listItem);
+    cachRemoveItem(id);
 });
 
 // This event removes all todo items
 server.on('removeAll', () => {
     list.innerHTML = '';
+    cachClear();
 });
 
 // This event checks todo by id
 server.on('check', (id) => {
     listItem = document.getElementById(id);
     listItem.className = (listItem.className == 'todo-checked') ? 'todo-unchecked' : 'todo-checked';
+    cachCheckItem(id);
 });
 
 // This event checks all todo items
 server.on('checkAll', () => {
     items = Array.from( document.getElementsByClassName('todo-unchecked') );
     items.forEach((item) => item.className = 'todo-checked');
+    cachCheckAll();
 });
+
+// NOTE: These are functions responsible for caching
+
+// Add todo to existing cach
+var cachAddItem = (todo) => {
+    var todosSaved = cachGet();
+    localStorage.setItem(todo._id, JSON.stringify(todo));
+};
+
+// Chek item by key
+var cachCheckItem = (key) => {
+    var item = JSON.parse(localStorage[key]);
+    item['isChecked'] = !item['isChecked'];
+    localStorage.setItem(key, JSON.stringify(item));
+};
+
+// Check all items
+var cachCheckAll = () => {
+    var cachedItems = cachGet().forEach((todo) => { cachCheckItem(todo._id) } );
+};
+
+//Get cached data
+var cachGet = () => {
+    var values = [], data = [];
+    var keys = Object.keys(localStorage).map((key) => parseInt(key));
+    keys.sort(function(a,b){return b - a}); // to enshure that the orer of items is kept
+    keys.map((key) => data.push(JSON.parse(localStorage.getItem(key))));
+    return data;
+};
+
+// Remove item by id
+var cachRemoveItem = (id) => {
+    localStorage.removeItem(id);
+};
+
+// Clear cach
+var cachClear = () => {
+    localStorage.clear();
+};
