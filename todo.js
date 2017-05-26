6f5c04e6c3fe2b02afffe4fb888e43c823e842e7
jamnab <jamesbmcnab@gmail.com>
2017-05-26 13:43:55 -0400

    fixed title assignment

--- todo.js
+++ todo.js
@@ -1,7 +1,8 @@
 // SyntaxError: Block-scoped declarations (let, const, function, class) not yet supported outside strict mode
 'use strict';
 module.export = class Todo {
-    constructor(title='') {
+    // constructor(title='') { SyntaxError: Unexpected token =
+    constructor(title) {
         this.title = title
     }
 }
