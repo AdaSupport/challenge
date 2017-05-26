64d07288e4c62cf5cb41c8c6e6da9924affb5e0b
jamnab <jamesbmcnab@gmail.com>
2017-05-26 13:32:40 -0400

    added use strict on todo

--- todo.js
+++ todo.js
@@ -1,3 +1,5 @@
+// SyntaxError: Block-scoped declarations (let, const, function, class) not yet supported outside strict mode
+'use strict';
 module.export = class Todo {
     constructor(title='') {
         this.title = title
