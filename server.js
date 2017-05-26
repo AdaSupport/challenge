6742ec029c97eb8ec2547ae6af91aac788d38440
Rohit <moza.rohit@gmail.com>
2017-05-26 18:17:56 -0400

    To-do list DB persists in server memory. New and re-connecting clients can access the entire current DB in server memory

--- server.js
+++ server.js
@@ -7,48 +7,57 @@ const io = require('socket.io')(server);
 const firstTodos = require('./data');
 const Todo = require('./todo');
 
-
 // Sends index.html to client's browser
 app.use(express.static(__dirname + '/public'));
 
 server.listen(3003);
 console.log('Waiting for clients to connect');
 
+
+let DB_index_count = 0;   //keeps track of DB_Index
+ // This is going to be our fake 'database' for this application
+// Parse all default Todo's from db
+
+// FIXED:DB not reloading on client refresh. It should be persistent on new client connections from the last time the server was run...
+const DB = firstTodos.map((t) => {
+    DB_index_count ++;
+    console.log("My DB_index_count:", DB_index_count);
+    // Form new Todo objects
+    return new Todo(title=t.title);
+
+});
+
+
+
+
+
+
 // On connection to sockets server
 io.on('connection', (client) => {
-    let DB_index_count = 0;   //keeps track of DB_Index
-    let DB_initial_count = 0;
     console.log('Client connected');
 
     client.on('join', function(data) {
         console.log(data);
 
     });
-    // This is going to be our fake 'database' for this application
-    // Parse all default Todo's from db
+    let onConnect = true;
 
-    // FIXME: DB is reloading on client refresh. It should be persistent on new client connections from the last time the server was run...
-    const DB = firstTodos.map((t) => {
-        DB_index_count ++;
-        DB_initial_count ++;
-        console.log("My DB_index_count:", DB_index_count);
-        // Form new Todo objects
-        return new Todo(title=t.title);
-
-    });
 
     // Sends a message to the client to reload all todos
     // Sending all to-dos for a first time connecting client
-    // Else
+    // Else send out the new to-do created
     const reloadTodos = () => {
-        if(DB_index_count < DB_initial_count + 1){
+
+        if(onConnect){
             client.emit('load', DB);
+            onConnect = false;
         } else {
+            DB_index_count ++;
             client.broadcast.emit('load', DB[DB_index_count-1]);
             client.emit('load', DB[DB_index_count-1]);
         }
 
-        DB_index_count ++;
+
         console.log("My DB_index_count:", DB_index_count);
 
     }
@@ -70,8 +79,6 @@ io.on('connection', (client) => {
     reloadTodos();
     client.on('disconnect', () => {
         console.log('disconnected client');
-        DB_index_count = 0;
-        DB_initial_count =0;
         console.log("My DB_index_count:", DB_index_count);
     });
 });
