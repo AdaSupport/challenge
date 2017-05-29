e8e0030f187736872852228881e3d673edd37e3a
a-void <avitiuk@me.com>
2017-05-29 10:31:07 -0400

    Add unit tests for server-side Redux store.
    Add test helper function to preload chai-immutable;

---
+++ test/server_test.js
@@ -0,0 +1,30 @@
+import { List, Map, fromJS } from 'immutable';
+import { createStore } from 'redux';
+import { expect } from 'chai';
+
+import { serverReducer } from '../src/reducers';
+
+describe('Redux store', () => {
+  it('is configured properly with the correct reducer', () => {
+    const store = createStore(serverReducer);
+
+    expect(store.getState()).to.equal(Map());
+
+    store.dispatch({
+      type: 'LOAD_TODOS',
+      todoList: [
+        {title: 'test0', id:0, completed: false},
+        {title: 'test1', id:1, completed: false},
+        {title: 'test2', id:2, completed: false}
+      ]
+    });
+
+    expect(store.getState()).to.equal(fromJS({
+      todoList: [
+        {title: 'test0', id:0, completed: false},
+        {title: 'test1', id:1, completed: false},
+        {title: 'test2', id:2, completed: false}
+      ]
+    }));
+  })
+});
