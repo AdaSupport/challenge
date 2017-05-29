b05d96459915a7597b3297faae7c9149adcb5cdf
a-void <avitiuk@me.com>
2017-05-28 23:36:29 -0400

    Rewrite main App.jsx as a dumb component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,58 +1,19 @@
 import React from 'react';
+
 import TodoForm from './TodoForm.jsx';
 import TodoList from './TodoList.jsx';
 import Header from './Header.jsx';
-import io from 'socket.io-client';
 
-const socket = io('http://localhost:3003/');
-
-export default class App extends React.PureComponent {
-  constructor() {
-    super()
-
-    this.state = {
-      todoList: []
-    }
-  }
-
-  // listen to server 'state' events and update the state accordingly
-  componentDidMount() {
-    socket.on('state', state => this.setState(state));
-  }
-
-  addTodo(title) {
-    return socket.emit('action', {type:'ADD_TODO', title});
-  }
-
-  deleteTodo(id) {
-    return socket.emit('action', {type:'DELETE_TODO', id});
-  }
-
-  completeTodo(id) {
-    return socket.emit('action', {type:'COMPLETE_TODO', id});
-  }
-
-  deleteAll() {
-    return socket.emit('action', {type:'DELETE_ALL'});
-  }
-
-  completeAll() {
-    socket.emit('action', {type:'COMPLETE_ALL'});
-  }
-
-  render() {
-    return (
-      <div>
-        <Header
-          completeAll={this.completeAll.bind(this)}
-          deleteAll={this.deleteAll.bind(this)} />
-        <TodoList
-          todoList={this.state.todoList || []}
-          deleteTodo={this.deleteTodo.bind(this)}
-          completeTodo={this.completeTodo.bind(this)} />
-        <TodoForm
-          addTodo={this.addTodo.bind(this)} />
-      </div>
-    )
-  }
-}
+const App = ({deleteAll, completeAll, todoList, deleteTodo, completeTodo, addTodo}) => (
+  <div>
+    <Header
+      deleteAll={deleteAll}
+      completeAll={completeAll} />
+    <TodoList
+      todoList={todoList || []}
+      deleteTodo={deleteTodo}
+      completeTodo={completeTodo} />
+    <TodoForm
+      addTodo={addTodo} />
+  </div>
+);
