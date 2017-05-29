1eb4f9e3c95a15e22c5a79c042662b5c2a781f28
a-void <avitiuk@me.com>
2017-05-29 00:56:57 -0400

    Configure TodoList component to manipulate Immutable data properly.

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -5,12 +5,12 @@ const TodoList = ({completeTodo, deleteTodo, todoList}) => (
     {
       todoList.map(todo => {
         return (
-          <li key={todo.id} className={todo.completed ? 'completed' : null}>
+          <li key={todo.get('id')} className={todo.get('completed') ? 'completed' : null}>
             <div
-              className={todo.completed ? 'checkbox checked' : 'checkbox'}
-              onClick={() => completeTodo(todo.id)}></div>
-            {todo.title}
-            <div className='close' onClick={() => deleteTodo(todo.id)}></div>
+              className={todo.get('completed') ? 'checkbox checked' : 'checkbox'}
+              onClick={() => completeTodo(todo.get('id'))}></div>
+            {todo.get('title')}
+            <div className='close' onClick={() => deleteTodo(todo.get('id'))}></div>
           </li>
         )
       })
