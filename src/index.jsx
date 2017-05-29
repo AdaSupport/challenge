395f2a86fa22e34a20fbcdefcb8738b1a5a9a0ec
a-void <avitiuk@me.com>
2017-05-29 00:15:56 -0400

    Create client-side Redux store and connect to the socket.io client.

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App.jsx';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import io from 'socket.io-client';
 
-render(<App />, document.getElementById('app'));
+import { AppContainer } from './components/App.jsx';
+import actionMiddleware from './actions/action_middleware';
+import { clientReducer } from './reducers';
+import { setState } from './actions';
+
+const socket = io('http://localhost:3003/');
+// creates a Redux store with a middleware function that emits actions to the socket.io server
+const createStoreWithMiddleware = applyMiddleware(actionMiddleware(socket))(createStore);
+const store = createStoreWithMiddleware(clientReducer);
+
+socket.on('state', state => store.dispatch(setState(state)));
+
+render(
+  <Provider store={store}>
+    <AppContainer />
+  </Provider>,
+  document.getElementById('app')
+)
