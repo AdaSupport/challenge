e15b311160325016533b4f807747dcd8c65428be
a-void <avitiuk@me.com>
2017-05-28 23:40:34 -0400

    Rewrite server-side Redux reducer to improve code readability.

--- src/reducers/serverReducer.js
+++ src/reducers/serverReducer.js
@@ -1,46 +1,33 @@
 import { List, Map } from 'immutable';
 import uuidV4        from 'uuid';
 
-// updates state with a new Todo based on the passed title and randomly generated id
-const addTodo = (state, title) => {
-  const newTodo = Map({title, id: uuidV4(), completed: false});
-  return state.updateIn(['todoList'], list => list.push(newTodo));
+const INITIAL_STATE = Map();
+
+// helper function to get the index of the Todo with assigned ID
+const getIndex = (state, id) => {
+  return state.get('todoList').findIndex(item => item.get('id') === id);
 }
 
-// sets {completed: true} for all Todos
-const completeAll = (state) => {
-  return state.updateIn(['todoList'], item => item.map(elem => elem.set('completed', true)));
-}
-
-// sets {completed: true} for a specified Todo
-const completeTodo = (state, id) => {
-  const index = state.get('todoList').findIndex(item => item.get('id') === id);
-  return state.setIn(['todoList', index, 'completed'], true);
-}
-
-// initiates the state with an empty Todo list
-const deleteAll = (state) => state.set('todoList', List());
-
-// deletes a Todo from the state
-const deleteTodo = (state, id) => {
-  const index = state.get('todoList').findIndex(item => item.get('id') === id);
-  return state.deleteIn(['todoList', index]);
-}
-
-// load initial Todos
-const loadTodos = (state, todoList) => {
-  return state.set('todoList', todoList);
-}
-
-// reducer function for Redux store
-const serverReducer = (state = Map(), action) => {
+const serverReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case 'ADD_TODO'     : return addTodo(state, action.title);
-    case 'COMPLETE_ALL' : return completeAll(state);
-    case 'COMPLETE_TODO': return completeTodo(state, action.id);
-    case 'DELETE_ALL'   : return deleteAll(state);
-    case 'DELETE_TODO'  : return deleteTodo(state, action.id);
-    case 'LOAD_TODOS'   : return loadTodos(state, action.todoList);
+    case 'ADD_TODO':
+      const newTodo = Map({title: action.title, id: uuidV4(), completed: false});
+      return state.updateIn(['todoList'], list => list.push(newTodo));
+
+    case 'COMPLETE_ALL':
+      return state.updateIn(['todoList'], item => item.map(elem => elem.set('completed', true)));
+
+    case 'COMPLETE_TODO':
+      return state.setIn(['todoList', getIndex(state, action.id), 'completed'], true);
+
+    case 'DELETE_ALL' :
+      return state.set('todoList', List());
+
+    case 'DELETE_TODO':
+      return state.deleteIn(['todoList', getIndex(state, action.id)]);
+
+    case 'LOAD_TODOS':
+      return state.set('todoList', action.todoList);
   }
 
   return state;
