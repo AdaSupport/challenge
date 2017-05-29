ec02f572458dcb9d198ffddad31546a120bfe5fe
a-void <avitiuk@me.com>
2017-05-29 11:11:05 -0400

    Add unit tests for Redux serverReducer.
    Reposition serverReducer switch items for better readability.

--- src/reducers/serverReducer.js
+++ src/reducers/serverReducer.js
@@ -1,7 +1,7 @@
 import { List, Map, fromJS } from 'immutable';
 import uuidV4 from 'uuid';
 
-const INITIAL_STATE = Map();
+const INITIAL_STATE = Map({todoList: List()});
 
 // helper function to get the index of the Todo with assigned ID
 const getIndex = (state, id) => {
@@ -10,24 +10,24 @@ const getIndex = (state, id) => {
 
 const serverReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case 'LOAD_TODOS':
+      return state.set('todoList', fromJS(action.todoList));
+
     case 'ADD_TODO':
       const newTodo = Map({title: action.title, id: uuidV4(), completed: false});
       return state.updateIn(['todoList'], list => list.push(newTodo));
 
-    case 'COMPLETE_ALL':
-      return state.updateIn(['todoList'], item => item.map(elem => elem.set('completed', true)));
+    case 'DELETE_TODO':
+      return state.deleteIn(['todoList', getIndex(state, action.id)]);
 
     case 'COMPLETE_TODO':
       return state.setIn(['todoList', getIndex(state, action.id), 'completed'], true);
 
+    case 'COMPLETE_ALL':
+      return state.updateIn(['todoList'], item => item.map(elem => elem.set('completed', true)));
+
     case 'DELETE_ALL' :
       return state.set('todoList', List());
-
-    case 'DELETE_TODO':
-      return state.deleteIn(['todoList', getIndex(state, action.id)]);
-
-    case 'LOAD_TODOS':
-      return state.set('todoList', fromJS(action.todoList));
   }
 
   return state;
