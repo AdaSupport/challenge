0477a6fca27ae2856626a46acaefb5918104ae23
chadd williams <chadd980@hotmail.com>
2017-05-31 06:44:44 -0400

    added click functionality to complete button

--- client/src/components/todo.js
+++ client/src/components/todo.js
@@ -3,12 +3,27 @@ import Button from 'react-bootstrap/lib/Button';
 
 
 class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { completed: false};
+    this.handleCompleteClick = this.handleCompleteClick.bind(this);
+  }
+
+  handleCompleteClick(){
+    this.setState({
+      completed: !this.state.completed
+    });
+  }
   render() {
+    let completedStyle = "inverse"
+    if(this.state.completed){
+      completedStyle = "primary"
+    }
     return (
       <div className="todo-div">
         <p className="todo">finish challenge</p>
         <div className="buttons">
-          <Button bsSize="xs" bsStyle="" type="button"><span className="glyphicon glyphicon-ok todo-button"></span></Button>
+          <Button onClick={this.handleCompleteClick} bsSize="xs" bsStyle={completedStyle} type="button"><span className="glyphicon glyphicon-ok todo-button"></span></Button>
           <Button bsSize="xs" bsStyle="danger" type="button"><span className="glyphicon glyphicon-remove todo-button"></span></Button>
         </div>
       </div>
