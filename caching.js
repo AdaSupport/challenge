4e67d106e4ff350a07b13a1c971acfb234f54bb7
Brandon Mowat <brandonmowat11@gmail.com>
2017-08-31 04:32:31 -0400

    Removes some tip 'tingzzz

--- caching.js
+++ caching.js
@@ -8,10 +8,4 @@ function getDB() {
   return JSON.parse(localStorage.getItem('DB'))
 }
 
-// get todo item with id
-function get(id) {}
-
-// insert todo item into cache
-function insert(todo) {}
-
 module.exports = { saveDB, getDB }
