3173010397f86d0fece79faff6c2cacc7e156202
Justin Starry <justin.m.starry@gmail.com>
2017-11-03 19:09:03 -0400

    Persist database changes to disk

--- database.js
+++ database.js
@@ -1,4 +1,5 @@
-const firstTodos = require('./data');
+const fs = require('fs');
+const data = require('./data');
 const Todo = require('./todo');
 
 class DB {
@@ -9,37 +10,55 @@ class DB {
   }
 
   init() {
-    firstTodos.map((todo) => this.newTodo(todo.title));
+    this.counter = data.counter;
+    this.todos = new Map(data.map);
   }
 
   newTodo(title) {
     const newTodo = new Todo(this.counter, title);
     this.todos.set(this.counter, newTodo);
     this.counter++;
+    this._sync();
     return newTodo;
   }
 
   updateTodo(todo) {
     this.todos.set(todo.id, todo);
+    this._sync();
     return todo;
   }
 
   deleteTodo(todo) {
     this.todos.delete(todo.id);
+    this._sync();
   }
 
   deleteAll() {
     this.todos.clear();
+    this._sync();
   }
 
   completeAll() {
     this.todos.forEach((todo) => todo.completed = true);
+    this._sync();
   }
 
   values() {
     const todoList = new Array();
     this.todos.forEach((todo) => todoList.push(todo));
+    this._sync();
     return todoList;
+  }
+
+  _sync() {
+    // async write database to disk
+    const data = {
+      counter: this.counter,
+      map: Array.from(this.todos.entries())
+    }
+    fs.writeFile('data.json', JSON.stringify(data), (err) => {
+      if (err) throw err;
+    });
   }
 }
 module.exports = DB;
