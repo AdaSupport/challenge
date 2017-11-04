35b4ae9c8e257847fc2e66e1f64a2f5324c47197
Justin Starry <justin.m.starry@gmail.com>
2017-11-04 13:44:02 -0400

    Use react for rendering UI

--- client.js
+++ client.js
@@ -1,144 +1,123 @@
 const server = io('http://localhost:3003/');
-const list = document.getElementById('todo-list');
 
-// Startup
-const TodoCache = loadCache();
-TodoCache.forEach((todo) => render(todo));
-disableInput();
+class TodoApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: this._loadCache(),
+      disabled: true
+    };
+    this.initServerCallbacks();
+  }
 
-// NOTE: These are all our globally scoped functions for interacting with the server
-// This function adds a new todo from the input
-function add() {
-    const input = document.getElementById('todo-input');
+  render() {
+    return React.createElement('div', {className: 'todo-app'},
+      React.createElement('div', {className: 'todo-header'}, 
+        React.createElement('input', {className: 'todo-input', type: 'text', disabled: this.state.disabled, placeholder: 'Add a todo...', autoFocus: true}, null),
+        React.createElement('input', {className: 'create-todo button', type: 'button', disabled: this.state.disabled, value: 'Make', onClick: this.createTodo}, null)),
+      React.createElement(TodoList, {todos: this.state.todos, server, disabled: this.state.disabled}, null),
+      React.createElement('div', {className: 'todo-footer'}, 
+        React.createElement('input', {className: 'button', type: 'button', disabled: this.state.disabled, value: 'Complete All', onClick: this.completeAll}, null),
+        React.createElement('input', {className: 'button', type: 'button', disabled: this.state.disabled, value: 'Delete All', onClick: this.deleteAll}, null)));
+  }
 
-    // Emit the new todo as some data to the server
-    server.emit('make', {
-        title : input.value
+  // Event Handlers
+  //
+  createTodo() {
+    const input = document.querySelector('.todo-input');
+    server.emit('make', { title : input.value });
+    input.value = '';
+    input.focus();
+  }
+
+  completeAll() {
+    server.emit('complete-all');
+  }
+
+  deleteAll() {
+    server.emit('delete-all');
+  }
+
+  // Server Callbacks
+  //
+  initServerCallbacks() {
+    // Prevent users from interacting when server is down
+    server.on('disconnect', () => {
+      this.setState({disabled: true});
     });
 
-    // Clear the input
-    input.value = '';
+    // Allow users to interact when connected to server
+    server.on('connect', () => {
+      this.setState({disabled: false});
+    });
 
-    // Refocus the element
-    input.focus();
-}
+    // Reload the entire list of todos from the server
+    server.on('load', (todos) => {
+      const map = new Map();
+      todos.map((todo) => map.set(todo.id, todo));
+      this.setState({todos: map});
+      this._saveCache(this.state.todos);
+    });
 
-// TODO use React for rendering
-function render(todo) {
-    const listItem = document.createElement('li');
-    listItem.className = 'todo-list-item';
-    listItem.dataset.id = todo.id;
+    // Receive new todo from server
+    server.on('new', (todo) => {
+      this.state.todos.set(todo.id, todo);
+      this.forceUpdate();
+      this._saveCache(this.state.todos);
+    });
 
-    const listItemSpan = document.createElement('span');
-    listItemSpan.className = 'todo-title';
-    if (todo.completed) listItemSpan.style.textDecoration = 'line-through';
-    else listItemSpan.style.textDecoration = 'none';
-    const listItemText = document.createTextNode(todo.title);
-    listItemSpan.appendChild(listItemText);
+    // Receive updated todo from server
+    server.on('updated-todo', (todo) => {
+      this.state.todos.set(todo.id, todo);
+      this.forceUpdate();
+      this._saveCache(this.state.todos);
+    });
 
-    const listItemCheckbox = document.createElement('input');
-    listItemCheckbox.className = 'todo-checkbox';
-    listItemCheckbox.type = 'checkbox';
-    listItemCheckbox.checked = todo.completed;
-    listItemCheckbox.onclick = (event) => {
-      todo.completed = event.target.checked;
-      server.emit('update', todo);
-    };
+    // Receive deleted todo from server
+    server.on('deleted-todo', (todo) => {
+      this.state.todos.delete(todo.id);
+      this.forceUpdate();
+      this._saveCache(this.state.todos);
+    });
+  }
 
-    const listItemDelete = document.createElement('input');
-    listItemDelete.className = 'delete-todo';
-    listItemDelete.type = 'button';
-    listItemDelete.value = 'x';
-    listItemDelete.onclick = () => {
-      server.emit('delete', todo);
-    }
+  // Helpers
+  //
+  _saveCache(todos) {
+    window.localStorage.todos = JSON.stringify(Array.from(todos.entries()));
+  }
 
-    listItem.appendChild(listItemCheckbox);
-    listItem.appendChild(listItemSpan);
-    listItem.appendChild(listItemDelete);
-    list.append(listItem);
-}
-
-function completeAll() {
-    server.emit('complete-all');
-}
-
-function deleteAll() {
-    server.emit('delete-all');
-}
-
-// Helper Methods
-
-function saveCache() {
-    window.localStorage.todos = JSON.stringify(Array.from(TodoCache.entries()));
-}
-
-function loadCache() {
+  _loadCache() {
     if (!window.localStorage.todos) return new Map();
     return new Map(JSON.parse(window.localStorage.todos));
+  }
 }
 
-function disableInput() {
-  [...document.querySelectorAll('input')].forEach((el) => {
-    el.disabled = true;
-  });
+class TodoList extends React.Component {
+
+  render() {
+    return React.createElement('ul', {className: 'todo-list'},
+      [...this.props.todos.values()].map((todo) => {
+        return React.createElement('li', {key: todo.id, className: 'todo-list-item'},
+          React.createElement('input', {type: 'checkbox', className: 'todo-checkbox', disabled: this.props.disabled, checked: todo.completed, onChange: this.checkboxClick.bind(this, todo.id)}, null),
+          React.createElement('span', {className: 'todo-title', style: {textDecoration: todo.completed ? 'line-through' : 'none'}}, todo.title),
+          React.createElement('input', {type: 'button', className: 'delete-todo', disabled: this.props.disabled, value: 'x', onClick: this.deleteClick.bind(this, todo.id)}, null));
+      })
+    );
+  }
+
+  checkboxClick(todoId, event) {
+    const todo = this.props.todos.get(todoId);
+    todo.completed = event.target.checked;
+    this.props.server.emit('update', todo);
+  }
+
+  deleteClick(todoId, event) {
+    this.props.server.emit('delete', this.props.todos.get(todoId));
+  }
 }
 
-function find(todo) {
-    return list.querySelector('[data-id="' + todo.id + '"]');
-}
-
-// Prevent users from interacting when connection is disabled
-server.on('disconnect', () => {
-  disableInput();
-});
-
-// Allow users to interact when connected to server
-server.on('connect', () => {
-  [...document.querySelectorAll('input')].forEach((el) => {
-    el.disabled = false;
-  });
-});
-
-// Reload the entire list of todos from the server
-server.on('load', (todos) => {
-    [...list.children].forEach((el) => list.removeChild(el));
-    todos.forEach((todo) => render(todo));
-    
-    // Update cache
-    TodoCache.clear();
-    todos.map((todo) => TodoCache.set(todo.id, todo));
-    saveCache();
-});
-
-// Receive new todo from server
-server.on('new', (todo) => {
-    render(todo);
-
-    // Update cache
-    TodoCache.set(todo.id, todo);
-    saveCache();
-});
-
-// Receive updated todo from server
-server.on('updated-todo', (todo) => {
-    const todoEl = find(todo);
-    todoEl.querySelector('input').checked = todo.completed;
-    const todoSpan = todoEl.querySelector('span');
-
-    if (todo.completed) todoSpan.style.textDecoration = 'line-through';
-    else todoSpan.style.textDecoration = 'none';
-
-    // Update cache
-    TodoCache.set(todo.id, todo);
-    saveCache();
-});
-
-// Receive deleted todo from server
-server.on('deleted-todo', (todo) => {
-    list.removeChild(find(todo));
-
-    // Update cache
-    TodoCache.delete(todo.id);
-    saveCache();
-});
+ReactDOM.render(
+  React.createElement(TodoApp),
+  document.getElementById('root')
+);
