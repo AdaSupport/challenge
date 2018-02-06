b59c6f0ba81d784080d9e06bfc598336547cb8f8
Yvone Huynh <sw.yvone.huynh@gmail.com>
2018-02-06 11:23:07 -0500

    added keys to legend items

--- dev/scripts/components/legend.js
+++ dev/scripts/components/legend.js
@@ -1,29 +1,29 @@
 import React from "react";
 
-const Legend=()=>{
+const Legend = () => {
     return (
         <ul className="legend-container" aria-hidden="true">
-            <li className="legend-item">
+            <li className="legend-item" key="1">
                 <i className="fas fa-check"></i>
                 <p>completed</p>
             </li>
 
-            <li className="legend-item">
+            <li className="legend-item" key="2">
                 <i className="fas fa-hourglass-start" ></i>
                 <p>incomplete</p>
             </li>
 
-            <li className="legend-item">
+            <li className="legend-item" key="3">
                 <i className="far fa-trash-alt"></i>
                 <p>Delete</p>
             </li>
-            
-            <li className="legend-item">
+
+            <li className="legend-item" key="4">
                 <i className="fas fa-square complete"></i>
                 <p>completed</p>
             </li>
 
-            <li className="legend-item">
+            <li className="legend-item" key="5">
                 <i className="fas fa-square incomplete"></i>
                 <p>to be completed</p>
             </li>
