8ad041297275ca215c9955c6e0b2e4da1b9af00d
Steven Zhao <stevenzhao0819@gmail.com>
2018-03-11 15:12:47 -0400

    Modify layout style

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import { Header, Container} from 'semantic-ui-react';
-const style = {
-  marginBottom:'5%'
-}
+import style from './style'
+
 
 export default class Layout extends Component {
   render() {
     return (
       <div >
-        <Header as='h1' content='Real Time Todo App' textAlign='center'/>
-        <Container text style={style}>
+        <Header as='h1' style={style.header} content='Real Time Todo App' textAlign='center'/>
+        <Container text style={style.container}>
           {this.props.children}
         </Container>
       </div>
