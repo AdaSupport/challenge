7d467092a30daee907acff98027441157bec585d
Srujan Gopu <srujanjack@gmail.com>
2018-07-03 13:26:23 -0500

    Added angular library and bootstrap for UI

--- client.js
+++ client.js
@@ -1,46 +1,35 @@
+let app = angular.module('TODOApp', []);
 const server = io('http://localhost:3003/');
-const list = document.getElementById('todo-list');
 let timeStamp = undefined;
 
-// NOTE: These are all our globally scoped functions for interacting with the server
-// This function adds a new todo from the input
-function add() {
-    console.warn(event);
-    const input = document.getElementById('todo-input');
+app.controller('test', function($scope) {
 
-    // Emit the new todo as some data to the server
-    server.emit('service', {
-        record: {title: input.value},
-        service : 'Add'
-    });
+    $scope.add = function() {
+        server.emit('service', {
+            record: {title: $scope.todo},
+            service : 'Add'
+        });
+        $scope.todo = '';
+        // TODO: refocus the element
+    };
 
-    // Clear the input
-    input.value = '';
-    // TODO: refocus the element
-}
+    server.on('load', (todos) => {
+        if((todos.oldTS == null && timeStamp == undefined) || timeStamp == todos.oldTS){
+            //first time load all or timestamp matched
+            if(todos.status == 'Add'){
+                if(timeStamp == undefined){
+                    $scope.todoList = [];
+                }
 
-function render(todo) {
-    console.log(todo);
-    const listItem = document.createElement('li');
-    const listItemText = document.createTextNode(todo.title);
-    listItem.appendChild(listItemText);
-    list.append(listItem);
-}
-
-// NOTE: These are listeners for events from the server
-// This event is for (re)loading the entire list of todos from the server
-server.on('load', (todos) => {
-
-
-    if((todos.oldTS == null && timeStamp == undefined) || timeStamp == todos.oldTS){
-        //first time load all or timestamp matched
-        if(todos.status == 'Add'){
-            todos.db.forEach((todo) => render(todo));
+                todos.db.forEach((todo) => {
+                    $scope.todoList.push(todo);
+                });
+                timeStamp = todos.currentTS;
+                $scope.$apply();
+            }
         }
-    }
-    else {
-        //connection lost
-    }
-
-
+        else {
+            //connection lost
+        }
+    });
 });
